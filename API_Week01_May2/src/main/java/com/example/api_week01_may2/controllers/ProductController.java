8802705b2da5ba8a3c433ed1c5a8f7539b0f9c87
Tran Quang Huy <107989088+tranquanghuy-09@users.noreply.github.com>
2024-01-19 01:11:33 +0700

    modify project

--- API_Week01_May2/src/main/java/com/example/api_week01_may2/controllers/ProductController.java
+++ API_Week01_May2/src/main/java/com/example/api_week01_may2/controllers/ProductController.java
@@ -6,12 +6,14 @@ import com.example.api_week01_may2.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
@@ -23,7 +25,7 @@ public class ProductController {
     }
     @GetMapping("/products/{id}")
     Product getUserById(@PathVariable long id){
-        User user = restTemplate.getForObject("http://localhost:8080/users/"+id, User.class);
+        User user = restTemplate.getForObject("http://localhost:8080/api/users/"+id, User.class);
         Product product = productRepository.findById(id).get();
         product.setUser(user);
         return product;
--- API_Week01_May2/src/main/java/com/example/api_week01_may2/controllers/UserControler.java
+++ API_Week01_May2/src/main/java/com/example/api_week01_may2/controllers/UserControler.java
@@ -4,11 +4,13 @@ import com.example.api_week01_may2.models.User;
 import com.example.api_week01_may2.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class UserControler {
     @Autowired
     private UserRepository userRepository;
